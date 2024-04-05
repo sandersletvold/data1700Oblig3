87e7a27fcb8234beb13becce3d3585971559fcf3
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-04-05 15:19:13 +0200

    Opprettet Spring prosjekt.

---
+++ src/test/java/oslomet/data1700/oblig3/Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.oblig3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
